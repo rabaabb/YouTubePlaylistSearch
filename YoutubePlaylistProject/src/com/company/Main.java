b975cdecba30a0f7f390741647b7323352ad4f77
Rabail Abbas <rabailabbas@berkeley.edu>
2021-08-11 19:59:19 -0700

    changed name

---
+++ YoutubePlaylistProject/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+	// write your code here
+        /**String genre = "atif aslam";
+        YoutubeSearchJava first = new YoutubeSearchJava(genre); */
+        InteractiveScannerClass trigger = new InteractiveScannerClass();
+
+    }
+}
