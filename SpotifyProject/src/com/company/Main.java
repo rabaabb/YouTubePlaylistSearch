577d055522543a29845b95838f3b1b2340d6a315
Rabail Abbas <rabailabbas@berkeley.edu>
2021-08-11 18:26:18 -0700

    finalizing code

--- SpotifyProject/src/com/company/Main.java
+++ SpotifyProject/src/com/company/Main.java
@@ -8,8 +8,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
 	// write your code here
-        String genre = "atif aslam";
-        YoutubeSearchJava first = new YoutubeSearchJava(genre);
+        /**String genre = "atif aslam";
+        YoutubeSearchJava first = new YoutubeSearchJava(genre); */
+        InteractiveScannerClass trigger = new InteractiveScannerClass();
 
     }
 }
